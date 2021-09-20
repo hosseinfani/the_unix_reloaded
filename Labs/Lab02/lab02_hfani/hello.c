863908f45153a7284030ead7b9d60bfed5ca2a08
hosseinfani <hosseinfani@gmail.com>
2021-09-20 17:05:52 -0400

    W02A

--- Labs/Lab02/lab02_hfani/hello.c
+++ Labs/Lab02/lab02_hfani/hello.c
@@ -1,5 +1,16 @@
 #include <stdio.h>
-int main(void){
-	printf("Hello World!\n");
-	return 0;
+#include <time.h>
+#include <stdlib.h>
+int main()
+{
+  printf("Hello World! This is hfani@uwindsor.ca, StudentID: 123456789\n");
+  time_t t = time(NULL);
+  struct tm tm = *localtime(&t);
+  printf("now: %d-%02d-%02d %02d:%02d:%02d\n", tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday, tm.tm_hour, tm.tm_min, tm.tm_sec);
+  printf("%s@shell:%s$\n", getenv("USER"),getenv("PWD"));
+
 }
+//int main(void){
+//	printf("Hello World!\n");
+//	return 0;
+//}
